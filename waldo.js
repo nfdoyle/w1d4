09bd95b3aa10460789525d81fe7b8878171564af
nicholas <ndoyle@uvic.ca>
2019-01-10 12:00:44 -0800

    refacted with forEach

--- waldo.js
+++ waldo.js
@@ -1,10 +1,13 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  /*for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {*/
+  arr.forEach(function(name) {
+    console.log(name);
+    if (name === "Waldo") {
+      found(arr.indexOf(name));   // execute callback
     }
-  }
+  })
 }
 
 function actionWhenFound(index) {
@@ -12,3 +15,10 @@ function actionWhenFound(index) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+/*var names = ['Steve', 'Tony', 'Bruce', 'Thor', 'etc.'];
+
+names.forEach(function(name) {
+  console.log(name);
+})
+*/
